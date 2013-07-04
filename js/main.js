37875fd657c26cca0b01f2a44229262669cb49e9
daniel <daniel@daniel-laptop.(none)>
2013-07-04 17:17:07 +0300

    main page is ready

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
-
+$(document).ready(function() {
+    
+    $('.home-page .preview-list a').on('click', function(e) {
+        
+        e.preventDefault();
+        
+        var source = $(this).find('img').attr('src');
+        
+        $('.home-page .preview-list li.active').removeClass('active');
+        $(this).closest('li').addClass('active');
+        $('.home-page .preview img').attr('src', $(this).find('img').attr('src'));
+        
+    });
+    
+});
